82dcf68152ebabc9e84117f19b777cbfd1a04abe
Zidane Ibrahim Fadela <zidaneibrhmfdl7@gmail.com>
2023-08-16 10:30:23 +0700

    upload data

---
+++ Bilangan Bulat dan Pecahan.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	
+	long int a, b;
+	float c;
+	scanf("%ld", &a, &b);
+	scanf("%f", &c);
+	printf("%d %f %d.\n," a, c, b);
+	
+	return 0;
+}
+
